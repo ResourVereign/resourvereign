ee93ec8d5c0185adb905f5abf447b167d75e8509
Dani Lupión <danilupion@gmail.com>
2025-01-03 21:11:44 +0100

    WIP

--- apps/server/src/utils/scheduler.ts
+++ apps/server/src/utils/scheduler.ts
@@ -1,19 +1,24 @@
 import { PluginType } from '@resourvereign/common/models/plugin.js';
-import config from 'config';
-import { endOfDay, startOfDay, subDays } from 'date-fns';
+import { addSeconds, endOfDay, startOfDay, subDays } from 'date-fns';
 import { scheduleJob } from 'node-schedule';
 
 import IntentModel, { IntentDocument } from '../models/intent.js';
 import {
   IntegrationUserPluginDocument,
   NotificationsUserPluginDocument,
+  SchedulingUserPluginDocument,
   UserPluginDocument,
 } from '../models/userPlugin.js';
 
 import { loggerForUser } from './logger.js';
-import { IntegrationPlugin, NotificationsPlugin, getPlugin } from './plugin.js';
-
-const schedulerCron = config.get<string>('scheduler.cron');
+import { MiddlewareChain } from './middlewareChain.js';
+import {
+  IntegrationPlugin,
+  NotificationsPlugin,
+  ScheduleMiddlewareContext,
+  SchedulingPlugin,
+  getPlugin,
+} from './plugin.js';
 
 const jobs = new Map<string, () => void>();
 
@@ -31,18 +36,47 @@ const getPluginInstance = async <UserPluginType extends UserPluginDocument>(
   if (plugin) {
     return (await plugin.initialize(
       { ...userPlugin.config },
-      loggerForUser(userPlugin.user.toString()),
+      loggerForUser(userPlugin.user.toString(), userPlugin.name),
     )) as UserPluginType extends IntegrationUserPluginDocument
       ? ReturnType<IntegrationPlugin['initialize']>
       : UserPluginType extends NotificationsUserPluginDocument
         ? ReturnType<NotificationsPlugin['initialize']>
-        : unknown;
+        : UserPluginType extends SchedulingUserPluginDocument
+          ? ReturnType<SchedulingPlugin['initialize']>
+          : unknown;
   }
 
   return null;
 };
 
-export const scheduleIntent = (intent: IntentDocument) => {
+const getNextDate = async (intent: IntentDocument) => {
+  await intent.integration.populate('addons');
+
+  const now = new Date();
+
+  const middlewareChain = new MiddlewareChain<ScheduleMiddlewareContext>();
+
+  // Loop through scheduling plugins and apply middleware
+  for (const notificationPlugin of intent.integration.addons.filter(
+    (addon) => addon.type === PluginType.Scheduling,
+  ) as SchedulingUserPluginDocument[]) {
+    const notificationInstance = await getPluginInstance(notificationPlugin);
+    if (notificationInstance) {
+      middlewareChain.use(notificationInstance.scheduleMiddleware);
+    }
+  }
+
+  const context: ScheduleMiddlewareContext = {
+    intent,
+    date: now,
+  };
+
+  await middlewareChain.execute(context);
+
+  return context.date;
+};
+
+export const scheduleIntent = async (intent: IntentDocument) => {
   // Cancel previous schedule if exists
   if (jobs.has(intent.id)) {
     const jobDisposer = jobs.get(intent.id)!;
@@ -53,54 +87,69 @@ export const scheduleIntent = (intent: IntentDocument) => {
     return;
   }
 
-  // Schedule job
-  const job = scheduleJob(schedulerCron, async () => {
-    const updatedIntent = await IntentModel.findOne({ _id: intent.id })
-      .populate('integration')
-      .exec();
+  const nextDate = await getNextDate(intent);
 
-    // If intent is not found, is satisfied or is scheduled for the past cancel job
-    if (!updatedIntent || !isIntentSchedulable(intent)) {
-      disposer();
-      return;
-    }
+  // TODO: limit past scheduling
 
-    const integrationInstance = await getPluginInstance(updatedIntent.integration);
+  console.log('nextDate: ', nextDate);
 
-    if (integrationInstance) {
-      const result = await integrationInstance.book(intent.date);
+  if (nextDate) {
+    // Schedule job
+    const job = scheduleJob(
+      new Date(Math.max(nextDate.getTime(), addSeconds(new Date(), 1).getTime())),
+      async () => {
+        const updatedIntent = await IntentModel.findOne({ _id: intent.id })
+          .populate('integration')
+          .exec();
 
-      if (result) {
-        updatedIntent.satisfied = true;
-        await updatedIntent.save();
-
-        // Loop through notification plugins and send notification
-        await updatedIntent.integration.populate('addons');
-        for (const notificationPlugin of updatedIntent.integration.addons.filter(
-          (addon) => addon.type === PluginType.Notifications,
-        ) as NotificationsUserPluginDocument[]) {
-          const notificationInstance = await getPluginInstance(notificationPlugin);
-
-          // TODO: improve notification message
-          await notificationInstance?.notify(
-            `${intent.integration.label} successfully booked for ${intent.date}`,
-          );
+        // If intent is not found, is satisfied or is scheduled for the past cancel job
+        if (!updatedIntent || !isIntentSchedulable(intent)) {
+          disposer();
+          return;
         }
-        disposer();
+
+        const integrationInstance = await getPluginInstance(updatedIntent.integration);
+
+        if (integrationInstance) {
+          const result = await integrationInstance.book(intent.date);
+
+          if (result) {
+            updatedIntent.satisfied = true;
+            await updatedIntent.save();
+
+            // Loop through notification plugins and send notification
+            await updatedIntent.integration.populate('addons');
+            for (const notificationPlugin of updatedIntent.integration.addons.filter(
+              (addon) => addon.type === PluginType.Notifications,
+            ) as NotificationsUserPluginDocument[]) {
+              const notificationInstance = await getPluginInstance(notificationPlugin);
+
+              // TODO: improve notification message
+              await notificationInstance?.notify(
+                `${intent.integration.label} successfully booked for ${intent.date}`,
+              );
+            }
+            disposer();
+          }
+        }
+      },
+    );
+
+    const disposer = () => {
+      // cancel job
+      if (job) {
+        job.cancel();
       }
+
+      // delete from map
+      jobs.delete(intent.id);
+    };
+
+    if (job) {
+      // Register disposer for intent
+      jobs.set(intent.id, disposer);
     }
-  });
-
-  const disposer = () => {
-    // cancel job
-    job.cancel();
-
-    // delete from map
-    jobs.delete(intent.id);
-  };
-
-  // Register disposer for intent
-  jobs.set(intent.id, disposer);
+  }
 };
 
 export const cancelIntent = (intent: IntentDocument) => {
